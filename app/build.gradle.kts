654876320df420cfbad19e2e032b8980c6bddcea
Stefan <stefan.zdra@gmail.com>
2026-06-21 21:21:59 +1200

    :bookmark: Prepare v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.histefanhere.actualwidgets"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
